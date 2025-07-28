707376880384065eecf4c166a8b1bfb259620ff9
furqan362 <furqanch472@gmail.com>
2025-07-28 07:05:06 -0700

    guess_change_2

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import random
 number = random.randint(1, 100)
-running = True
+
+attempts = 0  
+
 print(number)
-while running:
+
+while True:
     
     guess = int(input("Guess a number between 1 to 100: "))
+    attempts += 1  
+
     if guess == number:
-        print("Your guess is correct!")
-        running = False
+        print(f"Your guess is correct! You got it in {attempts} attempts!")
+        break
     
     elif guess > number:
         print("Your guess is greater than the number")
+    
     else:
         print("Your guess is less than the number")
-        
-print("Thank you for playing!")
+
+
