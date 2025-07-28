add618ce6af5775d6ac45d21751149a69a0b4dbb
furqan362 <furqanch472@gmail.com>
2025-07-28 08:10:19 -0700

    change_3

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import random
-number = random.randint(1, 100)
 
-attempts = 0  
+def guess_number():
 
-print(number)
+    number = random.randint(1, 100)
+    attempts = 0  
+
+    print(number)
+    while True:
+        try:
+            guess = int(input("Guess a number between 1 to 100: "))
+        except ValueError:
+            return "please enter correct value"
+            
+        attempts += 1  
+
+        if guess == number:
+            print(f"Your guess is correct! You got it in {attempts} attempts!")
+            break
+        
+        elif guess > number:
+            print("Your guess is greater than the number")
+        
+        else:
+            print("Your guess is less than the number")
 
 while True:
-    
-    guess = int(input("Guess a number between 1 to 100: "))
-    attempts += 1  
-
-    if guess == number:
-        print(f"Your guess is correct! You got it in {attempts} attempts!")
-        break
-    
-    elif guess > number:
-        print("Your guess is greater than the number")
-    
+    guess_number()
+    choise=input("enter e for exit: \n enter c for continue: ").lower()
+    if choise=="c":
+        continue
     else:
-        print("Your guess is less than the number")
-
-
+        exit
